b0b00cf446eeb4a51f2aa9b598914c6ad0144de1
Valentin Anikin <anival-94@mail.ru>
2021-11-14 19:23:12 +0300

    add solution

--- my_ciphering_cli.js
+++ my_ciphering_cli.js
@@ -1,129 +1,40 @@
-
-const readline = require('readline');
-const { parseOptions } = require('./lib/optionsValidation');
+const { parseCliOptions } = require('./lib/cliOptionsParser');
+const {
+    fromFileToFile,
+    fromFileToConsole,
+    fromConsoleToFile,
+    fromConsoleToConsole,
+} = require('./lib/processInput');
 
 const init = () => {
-    const { optionsMap, isReadFromFile, isWriteIntoFile } = parseOptions();
+    const cliOptions = parseCliOptions();
+
+    const {
+        isConfigPassed,
+        isInputPassed,
+        isOutputPassed,
+    } = cliOptions;
+
+    if (!isConfigPassed) {
+        return handleError('Config must be passed after the key "-c" or "--config"');
+    }
 
     switch (true) {
-        case isReadFromFile && isWriteIntoFile:
-            fromFileToFile(optionsMap);
-            return;
+        case isInputPassed && isOutputPassed:
+            return fromFileToFile(cliOptions);
 
-        case isReadFromFile && !isWriteIntoFile:
-            fromFileToConsole(optionsMap);
-            return;
+        case isInputPassed && !isOutputPassed:
+            return fromFileToConsole(cliOptions);
 
-        case !isReadFromFile && isWriteIntoFile:
-            fromConsoleToFile(optionsMap);
-            return;
+        case !isInputPassed && isOutputPassed:
+            return fromConsoleToFile(cliOptions);
 
-        case !isReadFromFile && !isWriteIntoFile:
-            fromConsoleToConsole(optionsMap);
-            return;
-    
+        case !isInputPassed && !isOutputPassed:
+            return fromConsoleToConsole(cliOptions);
+
         default:
-            break;
+            return handleError('Some error occured')
     }
 }
 
 init();
-
-
-
-// console.log(optionsMap);
-
-// let inputSteram;
-// let outputStream;
-
-// if (isThereInput) {
-//     const inputFromFile = optionsMap[CLI_OPTIONS.input.alias] || optionsMap[CLI_OPTIONS.input.name];
-
-//     inputSteram = fs.createReadStream(inputFromFile, 'utf8');
-// } else {
-//     const rl = readline.createInterface({
-//         input: process.stdin,
-//         output: process.stdout,
-//         prompt: '> ',
-//     });
-// }
-
-// process.stdin.on('data', (data) => {
-//     const dataStringified = data.toString();
-//     const encrypted = encript(dataStringified);
-//     process.stdout.write(encrypted);
-// })
-
-
-
-
-
-
-
-// const commands = [
-//     '-c',
-//     '--config',
-//     '-i',
-//     '--input',
-//     '-o',
-//     '--output',
-// ];
-
-
-// const readline = require('readline');
-
-
-// // rl.prompt();
-
-// // rl.question('Please, enter your text: ', (text) => {
-// //     console.log(text);
-// //     rl.close();
-// // })
-
-// // rl.on('line', (line) => {
-// //     const trimLine = line.trim();
-// // })
-
-// const processConfig = (config) => {
-
-// };
-
-// let src;
-
-// const processInput = (inputFile) => {
-//     fs.readFile(inputFile, 'utf8', (err, buffer) => {
-//         if (err) {
-//             console.error(err);
-//             process.exit(1);
-//         }
-
-//         src = buffer.toString();
-
-//     })
-
-//     const buffer = fs.readFileSync(inputFile, 'utf8');
-//     console.log(src);
-// };
-
-// const processOutput = (output) => {
-//     fs.writeFileSync(output, src) 
-// };
-
-// process.argv.forEach((command, index) => {
-//     const value = process.argv[index + 1];
-
-//     if (command === '-c' || command === '--config') {
-//         processConfig(value);
-//     }
-
-//     if (command === '-i' || command === '--input') {
-//         processInput(value);
-//     }
-
-//     if (command === '-o' || command === '--output') {
-//         processOutput(value);
-//     }
-// });
-  
-// process.exit(0);
-
